ab498f43bb9718073214d72aac744f77db306b19
unknown <admR@ACADEMY.TOP>
2025-12-15 12:59:54 +0300

    Models and starting db context class

--- WebStore/Data/ApplicationDBContext.cs
+++ WebStore/Data/ApplicationDBContext.cs
@@ -1,8 +1,31 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using WebStore.models;
 
 namespace WebStore.Data
 {
-    public class ApplicationDBContext
+    public class ApplicationDBContext : DbContext
     {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(8, 2)");
+            builder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products);
+
+        }
     }
 }
